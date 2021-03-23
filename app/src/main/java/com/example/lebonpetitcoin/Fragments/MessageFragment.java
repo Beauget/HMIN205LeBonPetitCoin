18e0d2ff110f41508b20cf5f04b298814b6b33c3
HayaatH <Hayaat.hebiret@live.fr>
2021-03-23 13:20:28 +0100

    test de reception de données firebase

--- app/src/main/java/com/example/lebonpetitcoin/Fragments/MessageFragment.java
+++ app/src/main/java/com/example/lebonpetitcoin/Fragments/MessageFragment.java
@@ -20,6 +20,7 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.events.Event;
 import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -37,10 +38,23 @@ public class MessageFragment extends Fragment {
     }
 
     TextView test;
+
     EditText eTitre;
     EditText eDescription;
-    Button button;
-    private FirebaseFirestore firestoreDB;
+
+    Button buttonSend;
+    Button buttonLoad;
+
+    TextView tLoad;
+
+    //RECUPERATION DE LA DB
+    private FirebaseFirestore firestoreDB = FirebaseFirestore.getInstance();
+
+    //RCUPERATION OU CREATION DU DOCUMENT MY FIRST NOTE
+    private DocumentReference noteReference = firestoreDB.collection("Note").document("My first note");
+    ////firestoreDB.document("Note/My first note"); équivalent de la ligne dessus
+
+
 
     //NOM DES ATTRIBUTS
     private static final String KEY_TITLE = "title";
@@ -51,25 +65,37 @@ public class MessageFragment extends Fragment {
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_message, container, false);
-        test= view.findViewById(R.id.collection);
+
+        //texte simple
+        test = view.findViewById(R.id.test);
         test.setText("Ci dessous les champs à remplir qui seront instancié dans le document My first note de la collection Note");
-        eTitre= view.findViewById(R.id.title);
-        button= view.findViewById(R.id.button);
-        eDescription = view.findViewById(R.id.description);
+
+        //Champs d'écritures
+        eTitre= view.findViewById(R.id.Etitle);
+        eDescription = view.findViewById(R.id.Edescription);
+
+        //boutton d'envoie du document
+        buttonSend= view.findViewById(R.id.buttonSend);
+        SendNote(view);
+
+        //boutton de chargement du document
+        buttonLoad= view.findViewById(R.id.buttonLoad);
+        LoadNote(view);
+
+        //Champs d'affichage du document
+        tLoad= view.findViewById(R.id.Tload);
+
 
         return view;
     }
 
     public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
 
-        //Gets a CollectionReference instance that refers to the collection at the specified path within the database.
-        //CollectionReference categorieRef = FirebaseFirestore.getInstance().collection("Categorie");
+    public void SendNote(View v){
 
-
-        //test.setText(firestoreDB.collection("Categorie").getId());
-        firestoreDB = FirebaseFirestore.getInstance();
-
-        button.setOnClickListener(new View.OnClickListener() {
+        buttonSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 /*
@@ -78,16 +104,16 @@ public class MessageFragment extends Fragment {
                 String title = eTitre.getText().toString();
                 String description = eDescription.getText().toString();
 
-
+                //Nommage des attributs
                 Map<String, Object> note = new HashMap<>();
                 note.put(KEY_TITLE, title);
                 note.put(KEY_DESCRIPTION, description);
 
                 /*
-                * CREATION DE LA COLLECTION NOTE ET D'UN DOCUMENT MY FIRST NOTE
-                */
-                //firestoreDB.document("Note/My first note"); équivalent a la ligne d'en dessous mais moins intuitif
-                firestoreDB.collection("Note").document("My first note").set(note)
+                 * CREATION DE LA COLLECTION NOTE ET D'UN DOCUMENT MY FIRST NOTE
+                 */
+
+                noteReference.set(note)
                         .addOnSuccessListener(new OnSuccessListener<Void>() {
                             @Override
                             public void onSuccess(Void aVoid) {
@@ -105,7 +131,42 @@ public class MessageFragment extends Fragment {
 
             }
         });
-        super.onActivityCreated(savedInstanceState);
+
+    }
+
+    public void LoadNote(View v){
+        buttonLoad.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                noteReference.get()
+                        .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                            @Override
+                            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                                if (documentSnapshot.exists()){
+                                    String titre = documentSnapshot.getString(KEY_TITLE);
+                                    String description = documentSnapshot.getString(KEY_DESCRIPTION);
+
+                                    //Map<String,Object> note = documentSnapshot.getData(); Si on veux récupérer les deux d'un coup
+
+                                    tLoad.setText("Titre : " + titre + "\n" + "Description : " + description);
+                                }
+                                else {
+                                    Toast.makeText(getContext(), "Le document n'existe pas", Toast.LENGTH_SHORT).show();
+                                }
+
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(getContext(),"erreur",Toast.LENGTH_SHORT).show();
+                                Log.d(TAG,e.toString());
+                            }
+                        });
+
+            }
+        });
+
     }
 }
 
