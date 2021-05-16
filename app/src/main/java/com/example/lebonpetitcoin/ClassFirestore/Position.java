82ecfe8108ac45d75021bc4454b3ea5b4bf1f577
HayaatH <Hayaat.hebiret@live.fr>
2021-05-16 18:19:18 +0200

    suite départements

--- app/src/main/java/com/example/lebonpetitcoin/ClassFirestore/Position.java
+++ app/src/main/java/com/example/lebonpetitcoin/ClassFirestore/Position.java
@@ -27,22 +27,93 @@ public class Position {
         liste.add(new departement("19","Corrèze",1.5237,45.2125));
         liste.add(new departement("2A","Corse-du-Sud",8.5917,41.5149));
         liste.add(new departement("2B","Haute-Corse",9.1223,42.2339));
-        liste.add(new departement("21","Côte-d'Or",4.4620,47.2529));
-        liste.add(new departement("22","",9.9999,99.9999));
-        liste.add(new departement("23","",9.9999,99.9999));
-        liste.add(new departement("24","",9.9999,99.9999));
-        liste.add(new departement("25","",9.9999,99.9999));
-        liste.add(new departement("26","",9.9999,99.9999));
-        liste.add(new departement("27","",9.9999,99.9999));
-        liste.add(new departement("28","",9.9999,99.9999));
-        liste.add(new departement("29","",9.9999,99.9999));
-        liste.add(new departement("30","",9.9999,99.9999));
-        liste.add(new departement("31","",9.9999,99.9999));
-        liste.add(new departement("32","",9.9999,99.9999));
-        liste.add(new departement("33","",9.9999,99.9999));
-        liste.add(new departement("34","",9.9999,99.9999));
-        liste.add(new departement("35","",9.9999,99.9999));
-        liste.add(new departement("36","",9.9999,99.9999));
+        liste.add(new departement("21","Cote-d'Or",4.4620,47.2529));
+        liste.add(new departement("22","Cote-d'Armor",2.5151,48.2628));
+        liste.add(new departement("23","Creuse",2.0108,46.0525));
+        liste.add(new departement("24","Dordogne",0.4429,45.0615));
+        liste.add(new departement("25","Doubs",6.2142,47.0955));
+        liste.add(new departement("26","Drome",5.1005,44.4103));
+        liste.add(new departement("27","Eure",0.5946,49.0649));
+        liste.add(new departement("28","Eure-et-Loir",1.2213,48.2315));
+        liste.add(new departement("29","Finistere",4.0332,48.1540));
+        liste.add(new departement("30","Gard",4.1049,43.5936));
+        liste.add(new departement("31","Haute-Garonne",1.1022,43.2131));
+        liste.add(new departement("32","Gers",0.2712,43.4134));
+        liste.add(new departement("33","Gironde",0.3431,44.4931));
+        liste.add(new departement("34","Herault",3.2202,43.3447));
+        liste.add(new departement("35","Ile-et-Vilaine",1.3819,48.0916));
+        liste.add(new departement("36","Indre",1.3433,46.4640));
+        liste.add(new departement("37","",9.9999,99.9999));
+        liste.add(new departement("38","",9.9999,99.9999));
+        liste.add(new departement("39","",9.9999,99.9999));
+        liste.add(new departement("40","",9.9999,99.9999));
+
+        liste.add(new departement("41","",9.9999,99.9999));
+        liste.add(new departement("42","",9.9999,99.9999));
+        liste.add(new departement("43","",9.9999,99.9999));
+        liste.add(new departement("44","",9.9999,99.9999));
+        liste.add(new departement("45","",9.9999,99.9999));
+        liste.add(new departement("46","",9.9999,99.9999));
+        liste.add(new departement("47","",9.9999,99.9999));
+        liste.add(new departement("48","",9.9999,99.9999));
+        liste.add(new departement("49","",9.9999,99.9999));
+
+        liste.add(new departement("50","",9.9999,99.9999));
+        liste.add(new departement("51","",9.9999,99.9999));
+        liste.add(new departement("52","",9.9999,99.9999));
+        liste.add(new departement("53","",9.9999,99.9999));
+        liste.add(new departement("54","",9.9999,99.9999));
+        liste.add(new departement("55","",9.9999,99.9999));
+        liste.add(new departement("56","",9.9999,99.9999));
+        liste.add(new departement("57","",9.9999,99.9999));
+        liste.add(new departement("58","",9.9999,99.9999));
+        liste.add(new departement("59","",9.9999,99.9999));
+
+        liste.add(new departement("60","",9.9999,99.9999));
+        liste.add(new departement("61","",9.9999,99.9999));
+        liste.add(new departement("62","",9.9999,99.9999));
+        liste.add(new departement("63","",9.9999,99.9999));
+        liste.add(new departement("64","",9.9999,99.9999));
+        liste.add(new departement("65","",9.9999,99.9999));
+        liste.add(new departement("66","",9.9999,99.9999));
+        liste.add(new departement("67","",9.9999,99.9999));
+        liste.add(new departement("68","",9.9999,99.9999));
+        liste.add(new departement("69","",9.9999,99.9999));
+
+        liste.add(new departement("70","",9.9999,99.9999));
+        liste.add(new departement("71","",9.9999,99.9999));
+        liste.add(new departement("72","",9.9999,99.9999));
+        liste.add(new departement("73","",9.9999,99.9999));
+        liste.add(new departement("74","",9.9999,99.9999));
+        liste.add(new departement("75","",9.9999,99.9999));
+        liste.add(new departement("76","",9.9999,99.9999));
+        liste.add(new departement("77","",9.9999,99.9999));
+        liste.add(new departement("78","",9.9999,99.9999));
+        liste.add(new departement("79","",9.9999,99.9999));
+
+        liste.add(new departement("80","",9.9999,99.9999));
+        liste.add(new departement("81","",9.9999,99.9999));
+        liste.add(new departement("82","",9.9999,99.9999));
+        liste.add(new departement("83","",9.9999,99.9999));
+        liste.add(new departement("84","",9.9999,99.9999));
+        liste.add(new departement("85","",9.9999,99.9999));
+        liste.add(new departement("86","",9.9999,99.9999));
+        liste.add(new departement("87","",9.9999,99.9999));
+        liste.add(new departement("88","",9.9999,99.9999));
+        liste.add(new departement("89","",9.9999,99.9999));
+
+        liste.add(new departement("90","",9.9999,99.9999));
+        liste.add(new departement("91","",9.9999,99.9999));
+        liste.add(new departement("92","",9.9999,99.9999));
+        liste.add(new departement("93","",9.9999,99.9999));
+        liste.add(new departement("94","",9.9999,99.9999));
+        liste.add(new departement("95","",9.9999,99.9999));
+
+
+
+
+
+
 
 
 
