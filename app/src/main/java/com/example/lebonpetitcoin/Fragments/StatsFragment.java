5b672ab27d57a86b36bfe61e2ca321cc3943578d
HayaatH <Hayaat.hebiret@live.fr>
2021-05-16 23:12:21 +0200

    visualisation des stats d'une annonce les 30 dernier jour

--- app/src/main/java/com/example/lebonpetitcoin/Fragments/StatsFragment.java
+++ app/src/main/java/com/example/lebonpetitcoin/Fragments/StatsFragment.java
@@ -19,16 +19,23 @@ import com.example.lebonpetitcoin.ClassFirestore.Compte;
 import com.example.lebonpetitcoin.ClassFirestore.Statistique;
 import com.example.lebonpetitcoin.MainActivity;
 import com.example.lebonpetitcoin.R;
+import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
 import com.github.mikephil.charting.formatter.PercentFormatter;
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
 import com.github.mikephil.charting.utils.ColorTemplate;
 import com.github.mikephil.charting.utils.MPPointF;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import android.graphics.Typeface;
+import android.widget.Toast;
+
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -38,6 +45,8 @@ import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
 public class StatsFragment extends Fragment {
 
@@ -50,8 +59,12 @@ public class StatsFragment extends Fragment {
 
     TextView textView;
     int nbVisualisation = 0;
-    ArrayList<PieEntry> entries = new ArrayList<>();
+    ArrayList<PieEntry> pieEntries = new ArrayList<>();
     PieChart pieChart;
+    ArrayList<BarEntry> barEntries = new ArrayList<>();
+    BarChart barChart;
+
+    int MAXJOUR = 30;
 
 
     public static StatsFragment newInstance() {
@@ -62,6 +75,7 @@ public class StatsFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_stats, container, false);
         textView = view.findViewById(R.id.textView);
         pieChart = view.findViewById(R.id.piechart);
+        barChart = view.findViewById(R.id.barchart);
 
         return view;
     }
@@ -73,7 +87,6 @@ public class StatsFragment extends Fragment {
     }
 
     public void getStatistiquePie(String pseudo){
-
         Task<QuerySnapshot> query = cAnnonces.whereEqualTo("auteur", pseudo).get();
         query.addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
@@ -81,16 +94,13 @@ public class StatsFragment extends Fragment {
                 if (task.isSuccessful()) {
                     for (QueryDocumentSnapshot document : task.getResult()) {
                         Annonce annonce = document.toObject(Annonce.class);
-                        entries.add(new PieEntry(annonce.getNbDeVisites(),annonce.getTitre()));
+                        pieEntries.add(new PieEntry(annonce.getNbDeVisites(),annonce.getTitre()));
                     }
-                    setPieChart(entries);
+                    setPieChart(pieEntries);
 
                 }
             }
         });
-
-
-
     }
 
     public void setPieChart(ArrayList<PieEntry> entries){
@@ -139,6 +149,54 @@ public class StatsFragment extends Fragment {
         pieChart.invalidate();
     }
 
+    public void getStatistiqueBar(String annonce){
+        Date now = Calendar.getInstance().getTime();
+        int [] array =new int[MAXJOUR];
+        Task<QuerySnapshot> query = cStatistique.whereEqualTo("idAnnonce", annonce).get();
+        query.addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        Statistique statistique= document.toObject(Statistique.class);
+                        Date date =statistique.getDate();
+
+                        int difference = (int) ((now.getTime()-date.getTime())/ (1000 * 3600 * 24));
+                        
+                        if(difference<MAXJOUR){
+                            array[difference]= array[difference] + 1;
+
+                        }
+                    }
+                    setBar(array);
+                }
+            }
+        });
+    }
+
+    public void setBar(int [] array ){
+
+        BarDataSet set;
+        int x = MAXJOUR -1;
+        for (int i=0; i<MAXJOUR; i++)
+        {
+            //BarEntryLabels.add("J-"+i)
+            barEntries.add(new BarEntry(x,array[i]));
+            x--;
+
+        }
+        ArrayList<IBarDataSet> dataSets = new ArrayList<>();
+        set = new BarDataSet(barEntries, " ");
+        //set.setColors(ColorTemplate.VORDIPLOM_COLORS);
+        set.setDrawValues(false);
+        dataSets.add(set);
+        BarData data = new BarData(dataSets);
+        barChart.setData(data);
+        barChart.setFitBars(false);
+        barChart.invalidate();
+
+    }
+
     public void getStatistique(String pseudo){
         nbVisualisation = 0;
 
@@ -169,7 +227,7 @@ public class StatsFragment extends Fragment {
                         Compte compte = document.toObject(Compte.class);
                         getStatistique(compte.getPseudo());
                         getStatistiquePie(compte.getPseudo());
-
+                        getStatistiqueBar("HdsvtjGULUKrIATwGqk8");
                     }
                 }
             }
